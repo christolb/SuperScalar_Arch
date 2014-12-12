dca5e599eb40979cf8b30c363850aad9fd0ae5c2
Christol Barnes <christolmb@gmail.com>
2014-12-11 23:18:08 -0800

    Added RAW dependency check

--- idu.v
+++ idu.v
@@ -6,7 +6,7 @@
 // Description : Instruction Dispatch Unit
 //
 ///////////////////////////////////////////////////////////////////////////////////////////
-module inst_dispatch #
+module idu #
 (
    parameter NUM_RS = 8, // Number of reservation stations in design
    parameter NUM_REG = 4, // Number of registers in the design
@@ -35,13 +35,13 @@ module inst_dispatch #
 
    // Instruction Dispatch Bus 1
    output [INS_PART_WID*3-1:0] instruction1, // <RS_TO_USE, SOURCE_1, SOURCE_2>
-   output instruction1_valid,
-   output [1:0] status_bus_tag1,
+   output                      instruction1_valid,
+   output [1:0]                status_bus_tag1,
 
    // Instruction Dispatch Bus 2
    output [INS_PART_WID*3-1:0] instruction2, // <RS_TO_USE, SOURCE_1, SOURCE_2>
-   output instruction2_valid,
-   output [1:0] status_bus_tag2,
+   output                      instruction2_valid,
+   output [1:0]                status_bus_tag2,
 
    // Done signals from reservation stations
    input [NUM_RS/4-1:0]     adder_done,
@@ -83,152 +83,163 @@ begin // {
    ////////////////////////////////////////////////////////////////////////////////////////
    // INSTRUCTION 1 DEPENDENCY CHECKS
    ////////////////////////////////////////////////////////////////////////////////////////
-   case(inst_1_type) // {
-   4'b0001: begin // ADD instruction
-               if(rs_status_table[ADD_0] != 1) begin // Check if ADD_0 RS is busy
-                   dispatch_inst1          = 1;
-                   inst1_rs                = ADD_0;
-                   rs_status_table[ADD_0]  = 1; // Make ADD_0 busy
-                   reg_status_table[inst_1_dest] = {ADD_0, 1'b1}; 
-               end else if(rs_status_table[ADD_1] != 1) begin // Check if ADD_1 is busy
-                   dispatch_inst1          = 1;
-                   inst1_rs                = ADD_1;
-                   rs_status_table[ADD_1]  = 1; // Make ADD_1 busy
-                   reg_status_table[inst_1_dest] = {ADD_1, 1'b1}; 
-               end else begin
-                   dispatch_inst1          = 0;
-                   inst1_rs                = 0;
+   if( (inst_1_src0 == instruction2_rs) || (inst_1_src1 == instruction2_rs) )  // RAW hazard check
+       inst1_stall = 1;
+   else
+       inst1_stall = 0;
+   if (!inst1_stall || inst2_to_inst1) begin // {
+      case(inst_1_type) // {
+      4'b0001: begin // ADD instruction
+                  if(rs_status_table[ADD_0] != 1) begin // Check if ADD_0 RS is busy
+                      dispatch_inst1          = 1;
+                      inst1_rs                = ADD_0;
+                      rs_status_table[ADD_0]  = 1; // Make ADD_0 busy
+                      reg_status_table[inst_1_dest] = {ADD_0, 1'b1}; 
+                  end else if(rs_status_table[ADD_1] != 1) begin // Check if ADD_1 is busy
+                      dispatch_inst1          = 1;
+                      inst1_rs                = ADD_1;
+                      rs_status_table[ADD_1]  = 1; // Make ADD_1 busy
+                      reg_status_table[inst_1_dest] = {ADD_1, 1'b1}; 
+                  end else begin
+                      dispatch_inst1          = 0;
+                      inst1_rs                = 0;
+                  end
                end
-            end
-   4'b0010: begin // MULT instruction
-               if(rs_status_table[MULT_0] != 1) begin // Check if MULT_0 RS is busy
-                   dispatch_inst1          = 1;
-                   inst1_rs                = MULT_0;
-                   rs_status_table[MULT_0]  = 1; // Make MULT_0 busy
-                   reg_status_table[inst_1_dest] = {MULT_0, 1'b1}; 
-               end else if(rs_status_table[MULT_1] != 1) begin // Check if MULT_1 is busy
-                   dispatch_inst1          = 1;
-                   inst1_rs                = MULT_1;
-                   rs_status_table[MULT_1]  = 1; // Make MULT_1 busy
-                   reg_status_table[inst_1_dest] = {MULT_1, 1'b1}; 
-               end else begin
-                   dispatch_inst1          = 0;
-                   inst1_rs                = 0;
+      4'b0010: begin // MULT instruction
+                  if(rs_status_table[MULT_0] != 1) begin // Check if MULT_0 RS is busy
+                      dispatch_inst1          = 1;
+                      inst1_rs                = MULT_0;
+                      rs_status_table[MULT_0]  = 1; // Make MULT_0 busy
+                      reg_status_table[inst_1_dest] = {MULT_0, 1'b1}; 
+                  end else if(rs_status_table[MULT_1] != 1) begin // Check if MULT_1 is busy
+                      dispatch_inst1          = 1;
+                      inst1_rs                = MULT_1;
+                      rs_status_table[MULT_1]  = 1; // Make MULT_1 busy
+                      reg_status_table[inst_1_dest] = {MULT_1, 1'b1}; 
+                  end else begin
+                      dispatch_inst1          = 0;
+                      inst1_rs                = 0;
+                  end
                end
-            end
-   4'b0011: begin // FETCH instruction
-               if(rs_status_table[FETCH_0] != 1) begin // Check if FETCH_0 RS is busy
-                   dispatch_inst1          = 1;
-                   inst1_rs                = FETCH_0;
-                   rs_status_table[FETCH_0]  = 1; // Make FETCH_0 busy
-                   reg_status_table[inst_1_dest] = {FETCH_0, 1'b1}; 
-               end else if(rs_status_table[FETCH_1] != 1) begin // Check if FETCH_1 is busy
-                   dispatch_inst1          = 1;
-                   inst1_rs                = FETCH_1;
-                   rs_status_table[FETCH_1]  = 1; // Make FETCH_1 busy
-                   reg_status_table[inst_1_dest] = {FETCH_1, 1'b1}; 
-               end else begin
-                   dispatch_inst1          = 0;
-                   inst1_rs                = 0;
+      4'b0011: begin // FETCH instruction
+                  if(rs_status_table[FETCH_0] != 1) begin // Check if FETCH_0 RS is busy
+                      dispatch_inst1          = 1;
+                      inst1_rs                = FETCH_0;
+                      rs_status_table[FETCH_0]  = 1; // Make FETCH_0 busy
+                      reg_status_table[inst_1_dest] = {FETCH_0, 1'b1}; 
+                  end else if(rs_status_table[FETCH_1] != 1) begin // Check if FETCH_1 is busy
+                      dispatch_inst1          = 1;
+                      inst1_rs                = FETCH_1;
+                      rs_status_table[FETCH_1]  = 1; // Make FETCH_1 busy
+                      reg_status_table[inst_1_dest] = {FETCH_1, 1'b1}; 
+                  end else begin
+                      dispatch_inst1          = 0;
+                      inst1_rs                = 0;
+                  end
                end
-            end
-   4'b0100: begin // STORE instruction
-               if(rs_status_table[STORE_0] != 1) begin // Check if STORE_0 RS is busy
-                   dispatch_inst1          = 1;
-                   inst1_rs                = STORE_0;
-                   rs_status_table[STORE_0]  = 1; // Make STORE_0 busy
-                   reg_status_table[inst_1_dest] = {STORE_0, 1'b1}; 
-               end else if(rs_status_table[STORE_1] != 1) begin // Check if STORE_1 is busy
-                   dispatch_inst1          = 1;
-                   inst1_rs                = STORE_1;
-                   rs_status_table[STORE_1]  = 1; // Make STORE_1 busy
-                   reg_status_table[inst_1_dest] = {STORE_1, 1'b1}; 
-               end else begin
-                   dispatch_inst1          = 0;
-                   inst1_rs                = 0;
+      4'b0100: begin // STORE instruction
+                  if(rs_status_table[STORE_0] != 1) begin // Check if STORE_0 RS is busy
+                      dispatch_inst1          = 1;
+                      inst1_rs                = STORE_0;
+                      rs_status_table[STORE_0]  = 1; // Make STORE_0 busy
+                      reg_status_table[inst_1_dest] = {STORE_0, 1'b1}; 
+                  end else if(rs_status_table[STORE_1] != 1) begin // Check if STORE_1 is busy
+                      dispatch_inst1          = 1;
+                      inst1_rs                = STORE_1;
+                      rs_status_table[STORE_1]  = 1; // Make STORE_1 busy
+                      reg_status_table[inst_1_dest] = {STORE_1, 1'b1}; 
+                  end else begin
+                      dispatch_inst1          = 0;
+                      inst1_rs                = 0;
+                  end
                end
-            end
-   default : begin // Illegal instruction
-                   dispatch_inst1          = 0;
-                   inst1_rs                = 0;
-                   reg_status_table[inst_1_dest] = 0; 
-             end
-   endcase // }
-
+      default : begin // Illegal instruction
+                      dispatch_inst1          = 0;
+                      inst1_rs                = 0;
+                      reg_status_table[inst_1_dest] = 0; 
+                end
+      endcase // }
+   end // }
    ////////////////////////////////////////////////////////////////////////////////////////
    // INSTRUCTION 2 DEPENDENCY CHECKS
    ////////////////////////////////////////////////////////////////////////////////////////
-   case(inst_2_type) // {
-   4'b0001: begin // ADD instruction
-               if(rs_status_table[ADD_0] != 1) begin // Check if ADD_0 RS is busy
-                   dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
-                   inst2_rs                = ADD_0;
-                   rs_status_table[ADD_0]  = 1; // Make ADD_0 busy
-                   reg_status_table[inst_2_dest] = {ADD_0, 1'b1}; 
-               end else if(rs_status_table[ADD_1] != 1) begin // Check if ADD_1 is busy
-                   dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
-                   inst2_rs                = ADD_1;
-                   rs_status_table[ADD_1]  = 1; // Make ADD_1 busy
-                   reg_status_table[inst_2_dest] = {ADD_1, 1'b1}; 
-               end else begin
-                   dispatch_inst2          = 0;
-                   inst2_rs                = 0;
+   if( (inst_2_src0 == instruction1_rs) || (inst_2_src1 == instruction1_rs) )  // RAW hazard check
+       inst2_stall = 1;
+   else 
+       inst2_stall = 1;
+   if(!inst2_stall) begin // {
+      case(inst_2_type) // {
+      4'b0001: begin // ADD instruction
+                  if(rs_status_table[ADD_0] != 1) begin // Check if ADD_0 RS is busy
+                      dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
+                      inst2_rs                = ADD_0;
+                      rs_status_table[ADD_0]  = 1; // Make ADD_0 busy
+                      reg_status_table[inst_2_dest] = {ADD_0, 1'b1}; 
+                  end else if(rs_status_table[ADD_1] != 1) begin // Check if ADD_1 is busy
+                      dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
+                      inst2_rs                = ADD_1;
+                      rs_status_table[ADD_1]  = 1; // Make ADD_1 busy
+                      reg_status_table[inst_2_dest] = {ADD_1, 1'b1}; 
+                  end else begin
+                      dispatch_inst2          = 0;
+                      inst2_rs                = 0;
+                  end
                end
-            end
-   4'b0010: begin // MULT instruction
-               if(rs_status_table[MULT_0] != 1) begin // Check if MULT_0 RS is busy
-                   dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
-                   inst2_rs                = MULT_0;
-                   rs_status_table[MULT_0]  = 1; // Make MULT_0 busy
-                   reg_status_table[inst_2_dest] = {MULT_0, 1'b1}; 
-               end else if(rs_status_table[MULT_1] != 1) begin // Check if MULT_1 is busy
-                   dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
-                   inst2_rs                = MULT_1;
-                   rs_status_table[MULT_1]  = 1; // Make MULT_1 busy
-                   reg_status_table[inst_2_dest] = {MULT_1, 1'b1}; 
-               end else begin
-                   dispatch_inst2          = 0;
-                   inst2_rs                = 0;
+      4'b0010: begin // MULT instruction
+                  if(rs_status_table[MULT_0] != 1) begin // Check if MULT_0 RS is busy
+                      dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
+                      inst2_rs                = MULT_0;
+                      rs_status_table[MULT_0]  = 1; // Make MULT_0 busy
+                      reg_status_table[inst_2_dest] = {MULT_0, 1'b1}; 
+                  end else if(rs_status_table[MULT_1] != 1) begin // Check if MULT_1 is busy
+                      dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
+                      inst2_rs                = MULT_1;
+                      rs_status_table[MULT_1]  = 1; // Make MULT_1 busy
+                      reg_status_table[inst_2_dest] = {MULT_1, 1'b1}; 
+                  end else begin
+                      dispatch_inst2          = 0;
+                      inst2_rs                = 0;
+                  end
                end
-            end
-   4'b0011: begin // FETCH instruction
-               if(rs_status_table[FETCH_0] != 1) begin // Check if FETCH_0 RS is busy
-                   dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
-                   inst2_rs                = FETCH_0;
-                   rs_status_table[FETCH_0]  = 1; // Make FETCH_0 busy
-                   reg_status_table[inst_2_dest] = {FETCH_0, 1'b1}; 
-               end else if(rs_status_table[FETCH_1] != 1) begin // Check if FETCH_1 is busy
-                   dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
-                   inst2_rs                = FETCH_1;
-                   rs_status_table[FETCH_1]  = 1; // Make FETCH_1 busy
-                   reg_status_table[inst_2_dest] = {FETCH_1, 1'b1}; 
-               end else begin
-                   dispatch_inst2          = 0;
-                   inst2_rs                = 0;
+      4'b0011: begin // FETCH instruction
+                  if(rs_status_table[FETCH_0] != 1) begin // Check if FETCH_0 RS is busy
+                      dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
+                      inst2_rs                = FETCH_0;
+                      rs_status_table[FETCH_0]  = 1; // Make FETCH_0 busy
+                      reg_status_table[inst_2_dest] = {FETCH_0, 1'b1}; 
+                  end else if(rs_status_table[FETCH_1] != 1) begin // Check if FETCH_1 is busy
+                      dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
+                      inst2_rs                = FETCH_1;
+                      rs_status_table[FETCH_1]  = 1; // Make FETCH_1 busy
+                      reg_status_table[inst_2_dest] = {FETCH_1, 1'b1}; 
+                  end else begin
+                      dispatch_inst2          = 0;
+                      inst2_rs                = 0;
+                  end
                end
-            end
-   4'b0100: begin // STORE instruction
-               if(rs_status_table[STORE_0] != 1) begin // Check if STORE_0 RS is busy
-                   dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
-                   inst2_rs                = STORE_0;
-                   rs_status_table[STORE_0]  = 1; // Make STORE_0 busy
-                   reg_status_table[inst_2_dest] = {STORE_0, 1'b1}; 
-               end else if(rs_status_table[STORE_1] != 1) begin // Check if STORE_1 is busy
-                   dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
-                   inst2_rs                = STORE_1;
-                   rs_status_table[STORE_1]  = 1; // Make STORE_1 busy
-                   reg_status_table[inst_2_dest] = {STORE_1, 1'b1}; 
-               end else begin
-                   dispatch_inst2          = 0;
-                   inst2_rs                = 0;
-               end
-            end 
-   default : begin // Illegal instruction
-                   dispatch_inst2          = 0;
-                   inst2_rs                = 0;
-                   reg_status_table[inst_2_dest] = 0; 
-             end
-   endcase //}
+      4'b0100: begin // STORE instruction
+                  if(rs_status_table[STORE_0] != 1) begin // Check if STORE_0 RS is busy
+                      dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
+                      inst2_rs                = STORE_0;
+                      rs_status_table[STORE_0]  = 1; // Make STORE_0 busy
+                      reg_status_table[inst_2_dest] = {STORE_0, 1'b1}; 
+                  end else if(rs_status_table[STORE_1] != 1) begin // Check if STORE_1 is busy
+                      dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
+                      inst2_rs                = STORE_1;
+                      rs_status_table[STORE_1]  = 1; // Make STORE_1 busy
+                      reg_status_table[inst_2_dest] = {STORE_1, 1'b1}; 
+                  end else begin
+                      dispatch_inst2          = 0;
+                      inst2_rs                = 0;
+                  end
+               end 
+      default : begin // Illegal instruction
+                      dispatch_inst2          = 0;
+                      inst2_rs                = 0;
+                      reg_status_table[inst_2_dest] = 0; 
+                end
+      endcase //}
+   end // }
 
 end // }
 

9ac19b171b7274e17712cf8114093ee48c10ba3b
Christol Barnes <christolmb@gmail.com>
2014-12-28 16:51:15 -0800

    Fixes to dispatch logic

--- idu.v
+++ idu.v
@@ -10,7 +10,8 @@ module idu #
 (
    parameter NUM_RS = 8, // Number of reservation stations in design
    parameter NUM_REG = 4, // Number of registers in the design
-   parameter CLK_DELAY = 2000, // Clock period in ns
+   parameter CLK_DELAY    = 2000, // Clock period in ns
+   parameter CLK_DELAYBY2 = 1000, // Half Clock period in ns
    parameter INS_PART_WID = 4, // Bitwidth of each part of instruction
    parameter TAG_LEN = 4 // Bit width of tags
 )
@@ -23,7 +24,7 @@ module idu #
    input [INS_PART_WID-1:0] inst_1_dest,
    input [INS_PART_WID-1:0] inst_1_src0,
    input [INS_PART_WID-1:0] inst_1_src1,
-   output inst_1_fetch,
+   output reg inst_1_fetch,
 
    // Instruction 2 components (Second Instruction)
    input inst_2_valid,
@@ -31,15 +32,15 @@ module idu #
    input [INS_PART_WID-1:0] inst_2_dest,
    input [INS_PART_WID-1:0] inst_2_src0,
    input [INS_PART_WID-1:0] inst_2_src1,
-   output inst_2_fetch,
+   output reg inst_2_fetch,
 
    // Instruction Dispatch Bus 1
-   output [INS_PART_WID*3-1:0] instruction1, // <RS_TO_USE, SOURCE_1, SOURCE_2>
+   output [INS_PART_WID*4-1:0] instruction1, // <RS_TO_USE, 4'b0000, SOURCE_1, SOURCE_2>
    output                      instruction1_valid,
    output [1:0]                status_bus_tag1,
 
    // Instruction Dispatch Bus 2
-   output [INS_PART_WID*3-1:0] instruction2, // <RS_TO_USE, SOURCE_1, SOURCE_2>
+   output [INS_PART_WID*4-1:0] instruction2, // <RS_TO_USE, 4'b0000, SOURCE_1, SOURCE_2>
    output                      instruction2_valid,
    output [1:0]                status_bus_tag2,
 
@@ -47,10 +48,13 @@ module idu #
    input [NUM_RS/4-1:0]     adder_done,
    input [NUM_RS/4-1:0]     mult_done,
    input [NUM_RS/4-1:0]     fetch_done,
-   input [NUM_RS/4-1:0]     store_done
+   input [NUM_RS/4-1:0]     store_done,
 
    // Register status table to outside logic
-   //output                   reg_status_table[0:NUM_REG] 
+   output reg [TAG_LEN+1-1:0]  reg1_status_table, 
+   output reg [TAG_LEN+1-1:0]  reg2_status_table, 
+   output reg [TAG_LEN+1-1:0]  reg3_status_table, 
+   output reg [TAG_LEN+1-1:0]  reg4_status_table 
 
 );
 
@@ -65,10 +69,30 @@ localparam STORE_0 = 4'b0111; localparam STORE_1 = 4'b1000;
 // for each reservation station
 reg rs_status_table[0:NUM_RS] ;
 
+assign rs_stat_0 = rs_status_table[1];
+assign rs_stat_1 = rs_status_table[2];
+assign rs_stat_2 = rs_status_table[3];
+assign rs_stat_3 = rs_status_table[4];
+assign rs_stat_4 = rs_status_table[5];
+assign rs_stat_5 = rs_status_table[6];
+assign rs_stat_6 = rs_status_table[7];
+assign rs_stat_7 = rs_status_table[8];
+
 // Register Status table: Stores the tag from
 // which each register obtains its value + status
 // bit which indicates whether it's busy
 reg [TAG_LEN+1-1:0] reg_status_table[0:NUM_REG] ;
+
+reg dispatch_inst1_d,dispatch_inst1_2d;
+reg dispatch_inst2_d,dispatch_inst2_2d;
+
+always @(*)
+begin
+   reg1_status_table = reg_status_table[0]-2;
+   reg2_status_table = reg_status_table[1]-2;
+   reg3_status_table = reg_status_table[2]-2;
+   reg4_status_table = reg_status_table[3]-2;
+end
 
 // Wires and signals
 reg [TAG_LEN-1:0] instruction1_src0, instruction1_src1, instruction2_src0, instruction2_src1;
@@ -77,16 +101,24 @@ reg               inst1_stall, inst2_stall;
 reg               dispatch_inst1, dispatch_inst2;
 reg [TAG_LEN-1:0] inst1_rs, inst2_rs;
 wire              inst2_to_inst1;
+integer           i;
+
+initial begin
+for( i =0; i<=NUM_RS; i=i+1)
+    rs_status_table[i] = 0;
+for( i =0; i<=NUM_REG; i=i+1)
+    reg_status_table[i] = 0;
+end
 
 // Instruction 1 and 2 : Check for dependencies
 always @(*)
 if(!clk)  // Negative phase of clock
 begin // {
    // Reset busy status for each reservation station upon completion of operation
-   rs_status_table[ADD_0]   = !adder_done[0]; rs_status_table[ADD_1]   = !adder_done[1];
-   rs_status_table[MULT_0]  = !mult_done[0] ; rs_status_table[MULT_1]  = !mult_done[1] ;
-   rs_status_table[FETCH_0] = !fetch_done[0]; rs_status_table[FETCH_1] = !fetch_done[1];
-   rs_status_table[STORE_0] = !store_done[0]; rs_status_table[STORE_1] = !fetch_done[1];
+   if(adder_done[0]) rs_status_table[ADD_0]   = 0;  if(adder_done[1]) rs_status_table[ADD_1]   = 0; 
+   if(mult_done[0])  rs_status_table[MULT_0]  = 0;  if(mult_done[1])  rs_status_table[MULT_1]  = 0;
+   if(fetch_done[0]) rs_status_table[FETCH_0] = 0;  if(fetch_done[1]) rs_status_table[FETCH_1] = 0;
+   if(store_done[0]) rs_status_table[STORE_0] = 0;  if(store_done[1]) rs_status_table[STORE_1] = 0;
 
    ////////////////////////////////////////////////////////////////////////////////////////
    // INSTRUCTION 1 DEPENDENCY CHECKS
@@ -105,13 +137,13 @@ begin // {
                       dispatch_inst1          = 1;
                       inst1_rs                = ADD_0;
                       rs_status_table[ADD_0]  = 1; // Make ADD_0 busy
-                      reg_status_table[inst_1_dest] = {ADD_0, 1'b1}; 
-                  end else if(rs_status_table[ADD_1] != 1) begin // Check if ADD_1 is busy
+                      reg_status_table[inst_1_dest-9] = {ADD_0, 1'b1}; 
+                  end else if(!dispatch_inst1 && rs_status_table[ADD_1] != 1) begin // Check if ADD_1 is busy
                       dispatch_inst1          = 1;
                       inst1_rs                = ADD_1;
                       rs_status_table[ADD_1]  = 1; // Make ADD_1 busy
-                      reg_status_table[inst_1_dest] = {ADD_1, 1'b1}; 
-                  end else begin
+                      reg_status_table[inst_1_dest-9] = {ADD_1, 1'b1}; 
+                  end else if (!dispatch_inst1) begin
                       dispatch_inst1          = 0;
                       inst1_rs                = 0;
                   end
@@ -121,13 +153,13 @@ begin // {
                       dispatch_inst1          = 1;
                       inst1_rs                = MULT_0;
                       rs_status_table[MULT_0]  = 1; // Make MULT_0 busy
-                      reg_status_table[inst_1_dest] = {MULT_0, 1'b1}; 
-                  end else if(rs_status_table[MULT_1] != 1) begin // Check if MULT_1 is busy
+                      reg_status_table[inst_1_dest-9] = {MULT_0, 1'b1}; 
+                  end else if(!dispatch_inst1 && rs_status_table[MULT_1] != 1) begin // Check if MULT_1 is busy
                       dispatch_inst1          = 1;
                       inst1_rs                = MULT_1;
                       rs_status_table[MULT_1]  = 1; // Make MULT_1 busy
-                      reg_status_table[inst_1_dest] = {MULT_1, 1'b1}; 
-                  end else begin
+                      reg_status_table[inst_1_dest-9] = {MULT_1, 1'b1}; 
+                  end else if (!dispatch_inst1) begin
                       dispatch_inst1          = 0;
                       inst1_rs                = 0;
                   end
@@ -137,13 +169,13 @@ begin // {
                       dispatch_inst1          = 1;
                       inst1_rs                = FETCH_0;
                       rs_status_table[FETCH_0]  = 1; // Make FETCH_0 busy
-                      reg_status_table[inst_1_dest] = {FETCH_0, 1'b1}; 
-                  end else if(rs_status_table[FETCH_1] != 1) begin // Check if FETCH_1 is busy
+                      reg_status_table[inst_1_dest-9] = {FETCH_0, 1'b1}; 
+                  end else if(!dispatch_inst1 && rs_status_table[FETCH_1] != 1) begin // Check if FETCH_1 is busy
                       dispatch_inst1          = 1;
                       inst1_rs                = FETCH_1;
                       rs_status_table[FETCH_1]  = 1; // Make FETCH_1 busy
-                      reg_status_table[inst_1_dest] = {FETCH_1, 1'b1}; 
-                  end else begin
+                      reg_status_table[inst_1_dest-9] = {FETCH_1, 1'b1}; 
+                  end else if (!dispatch_inst1) begin
                       dispatch_inst1          = 0;
                       inst1_rs                = 0;
                   end
@@ -153,13 +185,13 @@ begin // {
                       dispatch_inst1          = 1;
                       inst1_rs                = STORE_0;
                       rs_status_table[STORE_0]  = 1; // Make STORE_0 busy
-                      reg_status_table[inst_1_dest] = {STORE_0, 1'b1}; 
-                  end else if(rs_status_table[STORE_1] != 1) begin // Check if STORE_1 is busy
+                      reg_status_table[inst_1_dest-9] = {STORE_0, 1'b1}; 
+                  end else if(!dispatch_inst1 && rs_status_table[STORE_1] != 1) begin // Check if STORE_1 is busy
                       dispatch_inst1          = 1;
                       inst1_rs                = STORE_1;
                       rs_status_table[STORE_1]  = 1; // Make STORE_1 busy
-                      reg_status_table[inst_1_dest] = {STORE_1, 1'b1}; 
-                  end else begin
+                      reg_status_table[inst_1_dest-9] = {STORE_1, 1'b1}; 
+                  end else if (!dispatch_inst1) begin
                       dispatch_inst1          = 0;
                       inst1_rs                = 0;
                   end
@@ -167,7 +199,7 @@ begin // {
       default : begin // Illegal instruction
                       dispatch_inst1          = 0;
                       inst1_rs                = 0;
-                      reg_status_table[inst_1_dest] = 0; 
+                      reg_status_table[inst_1_dest-9] = 0; 
                 end
       endcase // }
    end // }
@@ -188,13 +220,13 @@ begin // {
                       dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
                       inst2_rs                = ADD_0;
                       rs_status_table[ADD_0]  = 1; // Make ADD_0 busy
-                      reg_status_table[inst_2_dest] = {ADD_0, 1'b1}; 
-                  end else if(rs_status_table[ADD_1] != 1) begin // Check if ADD_1 is busy
+                      reg_status_table[inst_2_dest-9] = {ADD_0, 1'b1}; 
+                  end else if(!dispatch_inst2 && rs_status_table[ADD_1] != 1) begin // Check if ADD_1 is busy
                       dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
                       inst2_rs                = ADD_1;
                       rs_status_table[ADD_1]  = 1; // Make ADD_1 busy
-                      reg_status_table[inst_2_dest] = {ADD_1, 1'b1}; 
-                  end else begin
+                      reg_status_table[inst_2_dest-9] = {ADD_1, 1'b1}; 
+                  end else if (!dispatch_inst2) begin
                       dispatch_inst2          = 0;
                       inst2_rs                = 0;
                   end
@@ -204,13 +236,13 @@ begin // {
                       dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
                       inst2_rs                = MULT_0;
                       rs_status_table[MULT_0]  = 1; // Make MULT_0 busy
-                      reg_status_table[inst_2_dest] = {MULT_0, 1'b1}; 
-                  end else if(rs_status_table[MULT_1] != 1) begin // Check if MULT_1 is busy
+                      reg_status_table[inst_2_dest-9] = {MULT_0, 1'b1}; 
+                  end else if(!dispatch_inst2 && rs_status_table[MULT_1] != 1) begin // Check if MULT_1 is busy
                       dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
                       inst2_rs                = MULT_1;
                       rs_status_table[MULT_1]  = 1; // Make MULT_1 busy
-                      reg_status_table[inst_2_dest] = {MULT_1, 1'b1}; 
-                  end else begin
+                      reg_status_table[inst_2_dest-9] = {MULT_1, 1'b1}; 
+                  end else if (!dispatch_inst2) begin
                       dispatch_inst2          = 0;
                       inst2_rs                = 0;
                   end
@@ -220,13 +252,13 @@ begin // {
                       dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
                       inst2_rs                = FETCH_0;
                       rs_status_table[FETCH_0]  = 1; // Make FETCH_0 busy
-                      reg_status_table[inst_2_dest] = {FETCH_0, 1'b1}; 
-                  end else if(rs_status_table[FETCH_1] != 1) begin // Check if FETCH_1 is busy
+                      reg_status_table[inst_2_dest-9] = {FETCH_0, 1'b1}; 
+                  end else if(!dispatch_inst2 && rs_status_table[FETCH_1] != 1) begin // Check if FETCH_1 is busy
                       dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
                       inst2_rs                = FETCH_1;
                       rs_status_table[FETCH_1]  = 1; // Make FETCH_1 busy
-                      reg_status_table[inst_2_dest] = {FETCH_1, 1'b1}; 
-                  end else begin
+                      reg_status_table[inst_2_dest-9] = {FETCH_1, 1'b1}; 
+                  end else if (!dispatch_inst2) begin
                       dispatch_inst2          = 0;
                       inst2_rs                = 0;
                   end
@@ -236,13 +268,13 @@ begin // {
                       dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
                       inst2_rs                = STORE_0;
                       rs_status_table[STORE_0]  = 1; // Make STORE_0 busy
-                      reg_status_table[inst_2_dest] = {STORE_0, 1'b1}; 
-                  end else if(rs_status_table[STORE_1] != 1) begin // Check if STORE_1 is busy
+                      reg_status_table[inst_2_dest-9] = {STORE_0, 1'b1}; 
+                  end else if(!dispatch_inst2 && rs_status_table[STORE_1] != 1) begin // Check if STORE_1 is busy
                       dispatch_inst2          = !dispatch_inst1 ? 1'b0 : 1; // Dispatch inst2 only if dispatch_inst1==1
                       inst2_rs                = STORE_1;
                       rs_status_table[STORE_1]  = 1; // Make STORE_1 busy
-                      reg_status_table[inst_2_dest] = {STORE_1, 1'b1}; 
-                  end else begin
+                      reg_status_table[inst_2_dest-9] = {STORE_1, 1'b1}; 
+                  end else if (!dispatch_inst2) begin
                       dispatch_inst2          = 0;
                       inst2_rs                = 0;
                   end
@@ -250,12 +282,18 @@ begin // {
       default : begin // Illegal instruction
                       dispatch_inst2          = 0;
                       inst2_rs                = 0;
-                      reg_status_table[inst_2_dest] = 0; 
+                      reg_status_table[inst_2_dest-9] = 0; 
                 end
       endcase //}
    end // }
 
 end // }
+
+assign status_bus_tag1[0] = !(instruction1_src1 >= 4'b1001 && instruction1_src1 <= 4'b1100); 
+assign status_bus_tag1[1] = !(instruction1_src0 >= 4'b1001 && instruction1_src0 <= 4'b1100); 
+
+assign status_bus_tag2[0] = !(instruction2_src1 >= 4'b1001 && instruction2_src1 <= 4'b1100); 
+assign status_bus_tag2[1] = !(instruction2_src0 >= 4'b1001 && instruction2_src0 <= 4'b1100); 
 
 // Dispatch logic
 always @(*)
@@ -263,20 +301,80 @@ if(clk) // Positive phase of clock
 begin
     if(dispatch_inst1)
     begin
-        instruction1_rs    = inst1_rs;
+        instruction1_rs   = inst1_rs;
         instruction1_src0 = inst_1_src0;
         instruction1_src1 = inst_1_src1;
     end
     if(dispatch_inst2)
     begin
-        instruction2_rs    = inst2_rs;
+        instruction2_rs   = inst2_rs;
         instruction2_src0 = inst_2_src0;
         instruction2_src1 = inst_2_src1;
     end
 end
 
 // Collect outputs
-assign instruction1 = {instruction1_rs, instruction1_src0, instruction1_src1}; 
-assign instruction2 = {instruction2_rs, instruction2_src0, instruction2_src1}; 
+assign instruction1 = {instruction1_rs, 4'b0000, instruction1_src0, instruction1_src1}; 
+assign instruction2 = {instruction2_rs, 4'b0000, instruction2_src0, instruction2_src1}; 
+
+// Generate valid signal
+always @(*)
+if(clk)
+begin
+    #CLK_DELAYBY2 dispatch_inst1_d  = dispatch_inst1;
+end
+
+always @(*)
+if(!clk)
+begin
+    #CLK_DELAYBY2 dispatch_inst1_2d = dispatch_inst1_d;
+end
+
+always @(*)
+if(clk)
+begin
+    #CLK_DELAYBY2 dispatch_inst2_d  = dispatch_inst2;
+end
+
+always @(*)
+if(!clk)
+begin
+    #CLK_DELAYBY2 dispatch_inst2_2d = dispatch_inst2_d;
+end
+
+assign instruction1_valid = dispatch_inst1 & !dispatch_inst1_d;
+assign instruction2_valid = dispatch_inst2 & !dispatch_inst2_d;
+
+initial begin
+    inst_1_fetch = 1;
+    #CLK_DELAY;
+    #CLK_DELAYBY2 inst_1_fetch = 0;
+end
+
+always @(*)
+if(clk)
+begin
+    if(dispatch_inst1)
+    begin
+        #CLK_DELAY inst_1_fetch = 1;
+        #CLK_DELAY inst_1_fetch = 0;
+    end
+end
+
+initial begin
+    inst_2_fetch = 1;
+    #CLK_DELAY;
+    #CLK_DELAYBY2 inst_2_fetch = 0;
+end
+
+always @(*)
+if(clk)
+begin
+    if(dispatch_inst2)
+    begin
+        #CLK_DELAY inst_2_fetch = 1;
+        #CLK_DELAY inst_2_fetch = 0;
+    end
+end
 
 endmodule
--- inst_queue.v
+++ inst_queue.v
@@ -1,11 +1,11 @@
 ///////////////////////////////////////////////////////////////////////////////////////////
-// Author    : CB
-// Date        : 12-04-2014
+// Author         : CB
+// Date           : 12-04-2014
 // Design Name    : inst_queue
 // Description    : Instruction Queue
-//                Latches two instructions on +ve cycle  : Instruction_1 and Instruction_2 
-//                Outputs two instructions on -ve cycle upon receiving fetch
-//                from IDU
+//                  Latches two instructions on +ve cycle  : Instruction_1 and Instruction_2 
+//                  Outputs two instructions on -ve cycle upon receiving fetch
+//                  from IDU
 //
 ///////////////////////////////////////////////////////////////////////////////////////////
 
@@ -23,40 +23,72 @@ parameter QUEUE_DEPTH  = 32
    input                        instruction_val_2,
    // Instruction 0
    input                        inst_1_fetch,
-   output                       inst_1_valid,
-   output [INS_PART_WID-1:0]    inst_type_1,
-   output [INS_PART_WID-1:0]    inst_dest_1,
-   output [INS_PART_WID-1:0]    inst_src0_1,
-   output [INS_PART_WID-1:0]    inst_src1_1,
+   output reg                       inst_1_valid,
+   output reg [INS_PART_WID-1:0]    inst_1_type,
+   output reg [INS_PART_WID-1:0]    inst_1_dest,
+   output reg [INS_PART_WID-1:0]    inst_1_src0,
+   output reg [INS_PART_WID-1:0]    inst_1_src1,
 
    // Instruction 1
    input                        inst_2_fetch,
-   output                       inst_2_valid,
-   output [INS_PART_WID-1:0]    inst_type_2,
-   output [INS_PART_WID-1:0]    inst_dest_2,
-   output [INS_PART_WID-1:0]    inst_src0_2,
-   output [INS_PART_WID-1:0]    inst_src1_2
+   output reg                       inst_2_valid,
+   output reg [INS_PART_WID-1:0]    inst_2_type,
+   output reg [INS_PART_WID-1:0]    inst_2_dest,
+   output reg [INS_PART_WID-1:0]    inst_2_src0,
+   output reg [INS_PART_WID-1:0]    inst_2_src1
 );
 
 // Generate the Queue for storing two instructions 
 reg [15:0]  instruction_q1[0:QUEUE_DEPTH-1];
+wire  [15:0]  instruction1_q_0;
+wire  [15:0]  instruction1_q_1;
+wire  [15:0]  instruction1_q_2;
+wire  [15:0]  instruction1_q_3;
 reg [15:0]  instruction_q2[0:QUEUE_DEPTH-1];
 
 localparam PTR_WIDTH = 5;
 
-reg [PTR_WIDTH-1:0] wr_ptr1, wr_ptr2;
-reg [PTR_WIDTH-1:0] rd_ptr1, rd_ptr2;
+reg [PTR_WIDTH-1:0] wr_ptr1=0, wr_ptr2=0;
+reg [PTR_WIDTH-1:0] rd_ptr1=0, rd_ptr2=0;
+
+initial begin
+   inst_1_valid = 0;
+   inst_1_type  = 0;
+   inst_1_dest  = 0;
+   inst_1_src0  = 0;
+   inst_1_src1  = 0;
+
+   inst_2_valid = 0;
+   inst_2_type  = 0;
+   inst_2_dest  = 0;
+   inst_2_src0  = 0;
+   inst_2_src1  = 0;
+end
+
+
+assign instruction1_q_0 = instruction_q1[0];
+assign instruction1_q_1 = instruction_q1[1];
+assign instruction1_q_2 = instruction_q1[2];
+assign instruction1_q_3 = instruction_q1[3];
 
 // Store the instructions into the Queue
 
-always @(*)
-if(clk)   // Latch the processor1 instruction on positive clk cycle
+always @(*)   // Latch the processor1 instruction on positive clk cycle
+if(clk)
 begin
    if(instruction_val_1)
    begin
-      instruction_q1  =  instruction_1;
-      wr_ptr1         =  wr_ptr1 + 1;
+      instruction_q1[wr_ptr1]  =  instruction_1;
    end
+end
+
+always @(*)
+if(clk)
+begin
+  if(instruction_val_1)
+  begin
+      wr_ptr1         =  wr_ptr1 + 1;
+  end
 end
 
 always @(*)
@@ -64,23 +96,49 @@ if(clk)   // Latch the processor1 instruction on positive clk cycle
 begin
    if(instruction_val_2)
    begin
-      instruction_q2  =  instruction_2;
+      instruction_q2[wr_ptr2]  =  instruction_2;
+   end
+end
+
+always @(*)
+if(clk)
+begin
+   if(instruction_val_2)
+   begin
       wr_ptr2         =  wr_ptr2 + 1;
    end
 end
+
+assign instruction_q1_empty = (wr_ptr1 == rd_ptr1);
 
 // Generate the outputs on receiving a fetch signal
 
 always @(*)
 if(!clk)
 begin
-   if(inst_1_fetch) // Fetch the instruction1 on negative clk cycle
+   if(inst_1_fetch & !instruction_q1_empty) // Fetch the instruction1 on negative clk cycle
    begin
-      inst_type_1 = instruction_q1[rd_ptr1][INS_PART_WID*4-1:-INS_PART_WID]; 
-      inst_dest_1 = instruction_q1[rd_ptr1][INS_PART_WID*3-1:-INS_PART_WID]; 
-      inst_src1_1 = instruction_q1[rd_ptr1][INS_PART_WID*2-1:-INS_PART_WID]; 
-      inst_src0_1 = instruction_q1[rd_ptr1][INS_PART_WID*1-1:-INS_PART_WID]; 
-      rd_ptr1     = rd_ptr1 + 1; 
+      inst_1_type = instruction_q1[rd_ptr1][15:12]; 
+   end
+end
+
+always @(*)
+if(!clk)
+begin
+   if(inst_1_fetch & !instruction_q1_empty) // Fetch the instruction1 on negative clk cycle
+   begin
+      inst_1_dest = instruction_q1[rd_ptr1][11:8]; 
+      inst_1_src1 = instruction_q1[rd_ptr1][7:4]; 
+      inst_1_src0 = instruction_q1[rd_ptr1][3:0]; 
+   end
+end
+
+always @(*)
+if(!clk)
+begin
+   if(inst_1_fetch & !instruction_q1_empty)
+   begin
+      #CLK_DELAY rd_ptr1     = rd_ptr1 + 1; 
    end
 end
 
@@ -88,21 +146,30 @@ end
 always @(*)
 if(!clk)
 begin
-   if(inst_1_fetch)
+   if(inst_1_fetch & !instruction_q1_empty)
        inst_1_valid = 1;
    #CLK_DELAY inst_1_valid = 0;
 end 
 
+assign instruction_q2_empty = (wr_ptr2 - rd_ptr2);
+
 always @(*)
 if(!clk)
 begin
-   if(inst_2_fetch) // Fetch the instruction2 on negative clk cycle
+   if(inst_2_fetch & !instruction_q2_empty) // Fetch the instruction2 on negative clk cycle
    begin
-      inst_type_2 = instruction_q2[rd_ptr2][INS_PART_WID*4-1:-INS_PART_WID]; 
-      inst_dest_2 = instruction_q2[rd_ptr2][INS_PART_WID*3-1:-INS_PART_WID]; 
-      inst_src1_2 = instruction_q2[rd_ptr2][INS_PART_WID*2-1:-INS_PART_WID]; 
-      inst_src0_2 = instruction_q2[rd_ptr2][INS_PART_WID*1-1:-INS_PART_WID]; 
-      rd_ptr2     = rd_ptr2 + 1; 
+      inst_2_type = instruction_q2[rd_ptr2][15:12]; 
+      inst_2_dest = instruction_q2[rd_ptr2][11:8]; 
+      inst_2_src1 = instruction_q2[rd_ptr2][7:4]; 
+      inst_2_src0 = instruction_q2[rd_ptr2][3:0]; 
+   end
+end
+
+always @(*)
+begin
+   if(inst_2_fetch & !instruction_q2_empty)
+   begin
+      #CLK_DELAY rd_ptr2     = rd_ptr2 + 1; 
    end
 end
 
